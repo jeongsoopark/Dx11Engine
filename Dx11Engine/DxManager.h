1583b7432d32a76df6b0dc2274a4c18c423e36f6
Jeongsoo Park <jeongsoop@nvidia.com>
2016-06-17 16:27:17 +0900

    no message

--- Dx11Engine/DxManager.h
+++ Dx11Engine/DxManager.h
@@ -7,34 +7,35 @@
 class DxManager
 {
 public:
-	DxManager();
-	~DxManager();
+    DxManager();
+    ~DxManager();
 
-	bool Init(int width, int height, bool vsync, HWND hwnd, bool fullscreen);
-	void BeginScene(float r, float g, float b, float a);
-	void EndScene();
+    bool Init(int width, int height, bool vsync, HWND hwnd, bool fullscreen);
+    void BeginScene(float r, float g, float b, float a);
+    void EndScene();
 
-	void EnableAlphaBlending(bool en);
-	void EnableZTest(bool en);
+    void EnableAlphaBlending(bool en);
+    void EnableZTest(bool en);
 
-	ID3D11Device* GetDevice();
-	ID3D11DeviceContext* GetDeviceContext();
+    ID3D11Device* GetDevice();
+    ID3D11DeviceContext* GetDeviceContext();
 
 private:
-	bool initSwapChain(HWND hwnd, bool fullscreen, int width, int height, UINT numerator, UINT denominator);
-	bool initDepthBuffer(int width, int height);
-	bool initDepthStencilBuffer();
-	bool initStencilView();
-	bool initRasterizerState();
-	bool initViewport(int width, int height);
+    bool initSwapChain(HWND hwnd, bool fullscreen, int width, int height, UINT numerator, UINT denominator);
+    bool initDepthBuffer(int width, int height);
+    bool initDepthStencilBuffer();
+    bool initStencilView();
+    bool initRasterizerState();
+    bool initViewport(int width, int height);
     bool initAlphaBlending();
 
     bool mVsync;
     int mGPUMemory;
     char* mGPUDescription[256];
-	IDXGISwapChain* mSwapChain;
-	ID3D11Device* mDevice;
-	ID3D11DeviceContext* mDeviceContext;
+    IDXGISwapChain* mSwapChain;
+    ID3D11Device* mDevice;
+    ID3D11DeviceContext* mDeviceContext;
+    :wa
 
 };
 
