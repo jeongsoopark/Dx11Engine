5beeb21bb40946d5ee9461944d7b37fa52601544
jeongsoo park <jeongsoop@JEONGSOOP-DT2.nvidia.com>
2016-06-18 23:51:54 +0900

    Writing dxmanager init function

--- Dx11Engine/DxManager.cpp
+++ Dx11Engine/DxManager.cpp
@@ -21,15 +21,116 @@ DxManager::DxManager()
 
 DxManager::~DxManager()
 {
+	if (mAlphaBlendStateEnabled)
+	{
+		mAlphaBlendStateEnabled->Release();
+		mAlphaBlendStateEnabled = 0;
+	}
+	if (mAlphaBlendStateDisabled)
+	{
+		mAlphaBlendStateDisabled->Release();
+		mAlphaBlendStateDisabled = 0;
+	}
+	if (mDepthStencilStateEnabled)
+	{
+		mDepthStencilStateEnabled->Release();
+		mDepthStencilStateEnabled = 0;
+	}
+	if (mDepthStencilStateDisabled)
+	{
+		mDepthStencilStateDisabled->Release();
+		mDepthStencilStateDisabled = 0;
+	}
+	if (mRasterizerState)
+	{
+		mRasterizerState->Release();
+		mRasterizerState = 0;
+	}
+	if (mDepthStencilView)
+	{
+		mDepthStencilView->Release();
+		mDepthStencilView = 0;
+	}
+	if (mDepthStencilBuffer)
+	{
+		mDepthStencilBuffer->Release();
+		mDepthStencilBuffer = 0;
+	}
+	if (mRenderTargetView)
+	{
+		mRenderTargetView->Release();
+		mRenderTargetView = 0;
+	}
+	if (mDeviceContext)
+	{
+		mDeviceContext->Release();
+		mDeviceContext = 0;
+	}
+	if (mDevice)
+	{
+		mDevice->Release();
+		mDevice = 0;
+	}
 	if (mSwapChain)
 	{
 		mSwapChain->SetFullscreenState(false, NULL);
 	}
+}
 
-	if (mAlphaBlendStateEnabled)
+bool DxManager::Init(int _width, int _height, bool _vsync, HWND _hwnd, bool _fullscreen)
+{
+	HRESULT res;
+	IDXGIFactory* factory;
+	IDXGIAdapter* adapter;
+	IDXGIOutput* adapterOutput;
+	UINT numModes, i, numerator, denominator, stringLength;
+	DXGI_MODE_DESC* displayModeList;
+	DXGI_ADAPTER_DESC* adapterDesc;
+	ID3D11Texture2D* backBuffer;
+	INT error;
+
+	//vsync setting copy
+	mVsync = _vsync;
+
+	//Create directx interface factor
+	res = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);
+	if (FAILED(res))
 	{
-		mAlphaBlendStateEnabled->Release();
+		MessageBox(NULL, "CreateDXGIFactory() failed", "Error", MB_OK);
+		return false;
 	}
+
+	res = factory->EnumAdapters(0, &adapter);
+	if (FAILED(res))
+	{
+		MessageBox(NULL, "EnumAdapters() failed", "Error", MB_OK);
+		return false;
+	}
+
+	res = adapter->EnumOutputs(0, &adapterOutput);
+	if (FAILED(res))
+	{
+		MessageBox(NULL, "EnumOutputs() failed", "Error", MB_OK);
+		return false;
+	}
+	
+	return false;
+}
+
+void DxManager::BeginScene(float r, float g, float b, float a)
+{
+}
+
+void DxManager::EndScene()
+{
+}
+
+void DxManager::EnableAlphaBlending(bool en)
+{
+}
+
+void DxManager::EnableZTest(bool en)
+{
 }
 
 bool DxManager::initSwapChain(HWND hwnd, bool fullscreen, int width, int height, UINT numerator, UINT denominator)
