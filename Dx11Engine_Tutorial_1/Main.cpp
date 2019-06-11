870947e4c9a2bd6eae9833adbb618fe6af7c6aac
jeongsoop <jeongsoo.park@gmail.com>
2019-06-11 23:02:45 +0900

    Add body of ~RenderWindow(), Initialize()

--- Dx11Engine_Tutorial_1/Main.cpp
+++ Dx11Engine_Tutorial_1/Main.cpp
@@ -1,5 +1,6 @@
 //Tutorial 1 Solution 2018-09-07 
 #include <Windows.h>
+#include "RenderWindow.h"
 #pragma comment(lib,"d3d11.lib")
 #pragma comment(lib,"DirectXTK.lib")
 
@@ -8,5 +9,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
+	RenderWindow rw;
+	rw.Initialize(hInstance, "my windows", "myclass", 800, 600);
 	return 0;
 }
--- Dx11Engine_Tutorial_1/RenderWindow.cpp
+++ Dx11Engine_Tutorial_1/RenderWindow.cpp
@@ -1,5 +1,5 @@
 #include "RenderWindow.h"
-
+#include "windows.h"
 
 
 RenderWindow::RenderWindow()
@@ -9,6 +9,11 @@ RenderWindow::RenderWindow()
 
 RenderWindow::~RenderWindow()
 {
+	if (this->mHandle != NULL)
+	{
+		UnregisterClass(this->mWindow_class.c_str(), this->mHInstance);
+		DestroyWindow(mHandle);
+	}
 }
 
 bool RenderWindow::Initialize(HINSTANCE hInstance, std::string windowTitle, std::string windowClass, int width, int height)
@@ -17,11 +22,54 @@ bool RenderWindow::Initialize(HINSTANCE hInstance, std::string windowTitle, std:
 	this->mWidth = width;
 	this->mHeight = height;
 	this->mWindow_title = windowTitle;
-	this->mWindow_class_wide = StringConverter::StringToWideString(this->mWindow_title);
+	this->mWindow_title_wide = StringConverter::StringToWideString(this->mWindow_title);
 	this->mWindow_class = windowClass;
-	this->mWindow_class_wide = StringConverter::StringToWideString(this->mWindow_title);
+	this->mWindow_class_wide = StringConverter::StringToWideString(this->mWindow_class);
 
+	//register window class
 	this->RegisterWindowsClass();
 
+	this->mHandle = CreateWindowEx(
+		0,
+		this->mWindow_class.c_str(),
+		this->mWindow_title.c_str(),
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		0,
+		0,
+		this->mWidth,
+		this->mHeight,
+		NULL,
+		NULL,
+		this->mHInstance,
+		nullptr);
+
+	if (this->mHandle == NULL) //in case of failure creating window
+	{
+		ErrLogger::Log(GetLastError(), "CreateWindowsEX failed");
+		return false;
+	}
+
+	ShowWindow(this->mHandle, SW_SHOW);
+	SetForegroundWindow(this->mHandle);
+	SetFocus(this->mHandle);
+
 	return false;
+}
+
+void RenderWindow::RegisterWindowsClass()
+{
+	WNDCLASSEX wc; 
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = this->mHInstance;
+	wc.hIcon = NULL;
+	wc.hIconSm = NULL;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = NULL;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = this->mWindow_class.c_str();
+	wc.cbSize = sizeof(WNDCLASSEX);
+	RegisterClassEx(&wc);
 }
